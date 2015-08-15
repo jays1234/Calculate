780bd6b79bf5c7c18e5d6faddae93f898263ad1c
jay <jays.phirayu@gmail.com>
2015-08-16 01:29:14 +0700

    complete

--- src/com/example/Main/Main.java
+++ src/com/example/Main/Main.java
@@ -1,95 +1,130 @@
 package com.example.Main;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by phirayu on 13/8/2558.
  */
 public class Main {
+    List<String> resultList = new ArrayList<String>();
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         //String text = input.nextLine();
-        String text = "1+2*4/5-7+3/6";
+        //String text = "1+2*4/5-7+3/6";
+
+        String text = "(((1+1)*2)*4)*3+10";
+
         System.out.println("input :"+text);
         Main main = new Main();
-        String postfix = main.infixToPostFix(text);
-        System.out.println("postfix :"+postfix);
-        System.out.println("answer : "+main.calculate(postfix));
+
+        main.infixToPostFix(text,0);
+        //[1, 1, 2, *, +]
+        //System.out.println("postfix :124*5/+736/+-");
+        System.out.println("postfix : " + main.resultList.toString());
+        Double answer=main.calculate(main.resultList);
+        if(answer == null) {
+            System.out.println("answer : Error" );
+        }else{
+            System.out.println("answer : "+answer);
+        }
 
 
 
 
     }
 
-    private Double calculate(String postfix){
+    private Double calculate(List<String> postfixs){
 
         String operation = "+-*/";
-        List<Integer> integers = new ArrayList<Integer>();
+        List<Integer> indexOperation = new ArrayList<Integer>();
 
-        for(int i=0;i<postfix.length();i++){
+        for(int i=0;i< postfixs.size();i++){
             for(int j=0;j<operation.length();j++){
-                if(postfix.charAt(i)==(operation.charAt(j))){
-                    integers.add(i);
+                if(postfixs.get(i).equals(operation.substring(j,j+1))){
+                    indexOperation.add(i);
                     break;
                 }
             }
         }
-        String[] postArray = postfix.split("");
-        List<String> postfixs = new ArrayList<String>(Arrays.asList(postArray));
-        postfixs.remove("");
+
         int count =0;
-        for(Integer i:integers){
-            int x= i-(count*2);
-            double z=0;
-            if (postfixs.get(x).equals("+")){
-                z = Double.parseDouble(postfixs.get(x - 2))+Double.parseDouble(postfixs.get(x - 1));
+        try {
+            for (Integer i : indexOperation) {
+                int x = i - (count * 2);
+                double z = 0;
+                if (postfixs.get(x).equals("+")) {
+                    z = Double.parseDouble(postfixs.get(x - 2)) + Double.parseDouble(postfixs.get(x - 1));
 
-            }else if(postfixs.get(x).equals("-")){
-                z = Double.parseDouble(postfixs.get(x - 2))-Double.parseDouble(postfixs.get(x - 1));
+                } else if (postfixs.get(x).equals("-")) {
+                    z = Double.parseDouble(postfixs.get(x - 2)) - Double.parseDouble(postfixs.get(x - 1));
 
-            }else if(postfixs.get(x).equals("*")){
-                z = Double.parseDouble(postfixs.get(x - 2))*Double.parseDouble(postfixs.get(x - 1));
+                } else if (postfixs.get(x).equals("*")) {
+                    z = Double.parseDouble(postfixs.get(x - 2)) * Double.parseDouble(postfixs.get(x - 1));
 
-            }else if(postfixs.get(x).equals("/")){
-                z = Double.parseDouble(postfixs.get(x - 2))/Double.parseDouble(postfixs.get(x - 1));
+                } else if (postfixs.get(x).equals("/")) {
+                    z = Double.parseDouble(postfixs.get(x - 2)) / Double.parseDouble(postfixs.get(x - 1));
 
-            }
-            List<String> temp = new ArrayList<String>();
-
-            for(int xx=0;xx<postfixs.size();xx++){
-                if(xx==x){
-                    temp.add(z+"");
-                }else if(xx == x-1 || xx== x-2){
-
-                }else{
-                    temp.add(postfixs.get(xx));
                 }
+                List<String> temp = new ArrayList<String>();
+
+                for (int xx = 0; xx < postfixs.size(); xx++) {
+                    if (xx == x) {
+                        temp.add(z + "");
+                    } else if (xx == x - 1 || xx == x - 2) {
+
+                    } else {
+                        temp.add(postfixs.get(xx));
+                    }
+                }
+                postfixs = temp;
+                count += 1;
+
             }
-            postfixs=temp;
-            count+=1;
 
+            return Double.parseDouble(postfixs.get(0));
+        }catch (Exception e){
+            return null;
         }
-
-        return Double.parseDouble(postfixs.get(0));
 
     }
 
-    private String infixToPostFix(String text){
+
+
+    private int infixToPostFix(String text,int count){
         String operation = "-+/*";
         List<String> strings = new ArrayList<String>();
+        //System.out.println(text);
         String result = "";
+        System.out.println(text);
         Boolean character ;
-        for(char a:text.toCharArray()){
+        for(int j=0;j<text.length();j++){
+
+            //System.out.println(resultList.toString());
+            /*if(count>=text.length()){
+                break;
+            }else if(text.charAt(j) != text.charAt(count)){
+                System.out.println(j+ ":"+count);
+                j=count;
+            }*/
+
+            char a=text.charAt(j);
+
+
             character=true;
+
+
             for(char oper:operation.toCharArray()){
                 if(a == oper){
                     character=false;
+                    if(result.length()>0) {
+                        resultList.add(result);
+                    }
+                    result="";
                     if(strings.size()>0){
                         while(strings.size()>0&&operation.indexOf(strings.get(strings.size()-1))>operation.indexOf(a)){
-                            result=result+strings.get(strings.size()-1);
+
+                            //result=result+strings.get(strings.size()-1);
+                            resultList.add(strings.get(strings.size()-1));
                             strings.remove(strings.get(strings.size()-1));
                         }
                         strings.add(a+"");
@@ -98,13 +133,35 @@ public class Main {
                     }
                 }
             }
-            if(character){
+            if(a == '('){
+                character=false;
+                j=infixToPostFix(text.substring(count+1),count);
+                count=j;
+
+
+            }else if(a == ')'){
+                //character=false;
+
+                if(result.length()>0) {resultList.add(result);}
+                for (int i=strings.size()-1;i>=0;i--){
+                    resultList.add(strings.get(i));
+                }
+                //System.out.println(text.substring(count));
+                return count+1;
+            }
+            else if(character){
                 result=result+a;
             }
+            count+=1;
+
+
+        }
+        if(result.length()>0) {
+            resultList.add(result);
         }
         for (int i=strings.size()-1;i>=0;i--){
-            result=result+strings.get(i);
+            resultList.add(strings.get(i));
         }
-        return result;
+        return count;
     }
 }
